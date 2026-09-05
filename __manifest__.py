616ca62e1628797bcf53c3957c2c3e63f734b5a7
unknown <fortunemakunya88@gmail.com>
2026-09-05 12:43:28 +0200

    added

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,7 @@
     'category': 'Sales/Point of Sale',
     'summary': 'Generate and monitor licenses for Desktop POS',
     'description': """
-        This module manages POS licenses, generating keys based on Machine IDs and monitoring their expiry.
+        This module manages POS licenses, generating keys based on Machine IDs and monitoring their expiry directly from Settings.
     """,
     'author': 'Havano',
     'depends': ['base', 'web'],
@@ -12,14 +12,7 @@
         'security/ir.model.access.csv',
         'views/license_views.xml',
     ],
-    'assets': {
-        'web.assets_backend': [
-            'pos_license_management/static/src/dashboard/dashboard.js',
-            'pos_license_management/static/src/dashboard/dashboard.xml',
-            'pos_license_management/static/src/dashboard/dashboard.scss',
-        ],
-    },
     'installable': True,
-    'application': True,
+    'application': False,
     'license': 'LGPL-3',
 }
--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,2 @@
 from . import pos_license
+from . import res_config_settings
